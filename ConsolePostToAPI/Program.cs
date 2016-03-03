152be37e2b8c2264d37006b1473ea8f3c879adda
meanprogrammer <mean.programmer.d@gmail.com>
2016-03-03 09:56:01 +0800

    1234

--- ConsolePostToAPI/Program.cs
+++ ConsolePostToAPI/Program.cs
@@ -14,7 +14,7 @@ namespace ConsolePostToAPI
         static void Main(string[] args)
         {
             /*
-            WebRequest request = WebRequest.Create("http://localhost:53172/api/values/post");
+            WebRequest request = WebRequest.Create("{0}api/values/post");
             request.Method = "POST";
             // Create POST data and convert it to a byte array.
             string postData = "=This is a test that posts this string to a Web server.";
@@ -46,32 +46,35 @@ namespace ConsolePostToAPI
             dataStream.Close();
             response.Close();
              */
-
+            string baseUrl = "http://dudzapi.apphb.com/";
             using (WebClient client = new WebClient())
             {
                 Console.WriteLine("========== STARTING GET VERB ==========");
                 client.Headers["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8";
-                var result = client.DownloadString("http://localhost:53172/api/values/get");
+                var result = client.DownloadString(string.Format("{0}api/values/get", baseUrl));
                 Console.WriteLine("GET NO PARAM = {0}", result);
                 Console.WriteLine("========== END GET VERB ==========");
+                Console.WriteLine(Environment.NewLine);
             }
 
             using (WebClient client = new WebClient())
             {
                 Console.WriteLine("========== STARTING GET VERB W/ PARAM ==========");
                 client.Headers["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8";
-                var result = client.DownloadString("http://localhost:53172/api/values/get/5");
+                var result = client.DownloadString(string.Format("{0}/api/values/get/5", baseUrl));
                 Console.WriteLine("GET WITH PARAM = {0}", result);
                 Console.WriteLine("========== END GET VERB W/ PARAM ==========");
+                Console.WriteLine(Environment.NewLine);
             }
 
             using (WebClient client = new WebClient())
             {
                 Console.WriteLine("========== STARTING POST VERB ==========");
                 client.Headers["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8";
-                var result = client.UploadString("http://localhost:53172/api/values/post", "=test test test");
+                var result = client.UploadString(string.Format("{0}api/values/post", baseUrl), "=test test test");
                 Console.WriteLine("POST = {0}", result);
                 Console.WriteLine("========== END POST VERB ==========");
+                Console.WriteLine(Environment.NewLine);
             }
             
 
@@ -80,18 +83,20 @@ namespace ConsolePostToAPI
             {
                 Console.WriteLine("========== STARTING PUT VERB ==========");
                 client.Headers["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8";
-                var result = client.UploadString("http://localhost:53172/api/values/put", "PUT", "=aaaa");
+                var result = client.UploadString(string.Format("{0}api/values/put", baseUrl), "PUT", "=aaaa");
                 Console.WriteLine("PUT = {0}", result);
                 Console.WriteLine("========== END PUT VERB ==========");
+                Console.WriteLine(Environment.NewLine);
             }
 
             using (WebClient client = new WebClient())
             {
                 Console.WriteLine("========== STARTING DELETE VERB ==========");
                 client.Headers["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8";
-                var result = client.UploadString("http://localhost:53172/api/values/delete", "DELETE", "=12");
+                var result = client.UploadString(string.Format("{0}api/values/delete", baseUrl), "DELETE", "=12");
                 Console.WriteLine("DELETE = {0}", result);
                 Console.WriteLine("========== END DELETE VERB ==========");
+                Console.WriteLine(Environment.NewLine);
             }
 
 
